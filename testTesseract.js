352d06a38769b03d2e5422f73a3f704b9a1c96b2
Bernard Ahn <bgamut@gmail.com>
2019-01-11 21:38:49 +0900

    testTesseract has been going through some updates

--- testTesseract.js
+++ testTesseract.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
 var spawn =require('child_process').spawn
 
+
 var inputPath = '/Users/bernardahn/Desktop/development/software/webapp/reader/temp/jpeg/jimptest.jpg'
 var language = 'ENG'
 /*
@@ -16,6 +17,10 @@ var args = [
     '-l',language
 ]
 var runningman = spawn('tesseract',args,{stdio:'ignore'})
+/*
 var englishtext = fs.readFileSync('/Users/bernardahn/Desktop/development/software/webapp/reader/temp/tesseractTest.txt','ascii')
 var koreantext = fs.readFileSync('/Users/bernardahn/Desktop/development/software/webapp/reader/temp/tesseractTest.txt','utf8')
+*/
+var englishtext = fs.readFileSync(__dirname+'/temp/tesseractTest.txt','ascii')
+var koreantext = fs.readFileSync(__dirname+'/temp/tesseractTest.txt','utf8')
 console.log(englishtext)
